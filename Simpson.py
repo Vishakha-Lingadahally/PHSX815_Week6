f55c51e27034b1b155c875c857384849536a79ec
Vishakha-Lingadahally <78513161+Vishakha-Lingadahally@users.noreply.github.com>
2021-03-10 00:02:55 +0100

    Add files via upload

---
+++ Simpson.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  9 01:08:39 2021
+
+@author: vishakha
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+def simps(f,a,b,N=50):
+    '''Approximate the integral of f(x) from a to b by Simpson's rule.
+
+    Simpson's rule approximates the integral \int_a^b f(x) dx by the sum:
+    (dx/3) \sum_{k=1}^{N/2} (f(x_{2i-2} + 4f(x_{2i-1}) + f(x_{2i}))
+    where x_i = a + i*dx and dx = (b - a)/N.
+
+    Parameters
+    ----------
+    f : function
+        Vectorized function of a single variable
+    a , b : numbers
+        Interval of integration [a,b]
+    N : (even) integer
+        Number of subintervals of [a,b]
+
+    Returns
+    -------
+    float
+        Approximation of the integral of f(x) from a to b using
+        Simpson's rule with N subintervals of equal length.
+
+    Examples
+    --------
+    >>> simps(lambda x : 3*x**2,0,1,10)
+    1.0
+    '''
+    if N % 2 == 1:
+        raise ValueError("N must be an even integer.")
+    dx = (b-a)/N
+    x = np.linspace(a,b,N+1)
+    y = f(x)
+    S = dx/3 * np.sum(y[0:-1:2] + 4*y[1::2] + y[2::2])
+    return S
